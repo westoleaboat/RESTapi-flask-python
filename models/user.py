6d9b5bc541fa64234a37d5d0901a2b82ca133f5d
atlasatlas <68698872+westoleaboat@users.noreply.github.com>
2022-12-23 23:33:56 +0200

    mailgun send message when registering user

--- models/user.py
+++ models/user.py
@@ -7,6 +7,7 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
 
     def json(self):
